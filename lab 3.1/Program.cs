2cdef6346d82da18c45b5daa3686eaf115f67a12
Илья <@DESKTOP-VPI29MR>
2024-10-18 10:39:44 +0200

    Добавьте файлы проекта.

---
+++ lab 3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿try
+{
+    int sum = 0; 
+    int n; 
+
+    Console.WriteLine("Введите числа (введите 0 для завершения):");
+
+    while (true)
+    {
+        
+        if (int.TryParse(Console.ReadLine(), out n))
+        {
+            
+            if (n == 0)
+            {
+                break;
+            }
+
+            
+            if (n < 0)
+            {
+                sum += n;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Пожалуйста, введите целое число.");
+        }
+    }
+
+    Console.WriteLine("Сумма отрицательных чисел: " + sum);
+}
+catch
+{
+
+}
